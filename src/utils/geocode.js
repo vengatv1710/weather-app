5f6b6750ad7bff2c018b72d52f078a5f55206152
vengatv1710 <vengatv2000@gmail.com>
2023-08-24 21:51:25 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://geocode.maps.co/search?q=' + address + '&limit=1';
+    request({url:url, json:true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to the server', undefined)
+        }else if (!body[0].lat){
+            callback('Unable to find the location try another location', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body[0].lat,
+                longitude: body[0].lon,
+                location: body[0].display_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
---
+++ src/utils/weather.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude='+ lat +'&longitude='+ long + '&current_weather=true'
+
+    request({url:url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to the server', undefined)
+        }else if (!body[0].lat){
+            callback('Unable to find the location try another location', undefined)
+        } else {
+            callback(undefined, {
+                temperature: body.current_weather.temperature,
+                windSpeed: body.current_weather.windspeed,
+                time: body.current_weather.time
+            })
+        }
+    })
+}
+
+module.exports = forecast
